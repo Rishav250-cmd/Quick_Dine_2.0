7456bf55d12d1c0b2f59ea63a8c25bfb3570c027
Rishav250-cmd <rishavdev.05@gmail.com>
2026-08-14 18:40:37 +0530

    creating api for authentication

--- Server/server.ts
+++ Server/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authroutes.js";
@@ -17,6 +17,14 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Server is Live!");
 });
 app.use("/api/auth" , authRouter);
+
+//global error handler 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Something went wrong!" , 
+        stack:process.env.NODE_ENV == "production" ? undefined:err.stack
+     });
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
