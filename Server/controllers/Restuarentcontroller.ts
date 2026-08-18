24bccc4da39809b0a40a8a6a6e81107578681325
Rishav250-cmd <rishavdev.05@gmail.com>
2026-08-18 21:50:36 +0530

    adding restuarent controller

--- Server/controllers/Restuarentcontroller.ts
+++ Server/controllers/Restuarentcontroller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { array } from "node:stream/iter";
 import { Restuarent } from "../models/restuarent.js";
 import jwt from "jsonwebtoken";       
+import { user } from "../models/users.js";
+import { Booking } from "../models/booking.js";
 
 export const getrestuarent = async(req:Request , res : Response):Promise<void>=>{
     try {
@@ -43,7 +45,6 @@ export const getrestuarent = async(req:Request , res : Response):Promise<void>=>
     }
 
 }
-
 export const getFeaturedrestuarent = async(req:Request , res : Response):Promise<void>=>{
     try {
         const featured = await Restuarent.find({
@@ -59,7 +60,6 @@ export const getFeaturedrestuarent = async(req:Request , res : Response):Promise
     }
 
 }
-
 export const getrestuarentbyslug = async(req:Request , res : Response):Promise<void>=>{
     try {
         const restuarant = await Restuarent.findOne({slug:req.params.slug});
@@ -75,17 +75,24 @@ export const getrestuarentbyslug = async(req:Request , res : Response):Promise<v
                     const token = req.headers.authorization.split(" ")[1];
                     const decoded = await jwt.verify(token , process.env.JWT_SECRET as string)as {id:string};
                     
+                    const User = await user.findById(decoded.id);
+                    if(user && (User?.role === "admin" )|| (User?.role==="owner" && restuarant.owner.toString()=== User._id.toString())){
+                        isAuthorized = true;
+
+                    }
                 }
                 catch(error){
                     console.log(error);
                     res.status(401).json({message:"Unauthorized"});
                     return;
                 }
-            }else{
-                res.status(401).json({message:"Unauthorized"});
-                return;
+            }
+            if(!isAuthorized){
+                res.status(404).json({message:"Restuarent not find or pending approval"})
+                return ;
             }
         }
+        res.json(restuarant)
     } catch (error : any) {
         console.log(error);
         res.status(400).json({message:error.message});
@@ -94,11 +101,40 @@ export const getrestuarentbyslug = async(req:Request , res : Response):Promise<v
 }
 export const getrestuarentAvailability = async(req:Request , res : Response):Promise<void>=>{
     try {
-        
-        
+        const {date} = req.query;
+        if(!date){
+            res.status(400).json({message:"please provide a date"})
+            return;
+        }
+        const restuarent = await Restuarent.findById(req.params.id);
+        if(!restuarent){
+            res.status(400).json({message:"Restuarent not found"})
+            return;
+        }
+        const bookingdate = new Date(date as string) 
+
+        //get all the active booking on this date .
+        const booking = await Booking.find({
+            restaurant : restuarent._id,
+            date : bookingdate ,
+            status:"confirmed"
+
+        })
+        //map slot to availabale slot 
+        const availability = restuarent.availableSlots.map((slot)=>{
+            const bookedseat = booking.filter((b)=>b.time===slot).reduce((sum, b )=> sum+b.guests,0)
+            const totalseats = restuarent.totalseats || 20 ;
+            const availableseat = Math.max(0 , totalseats-bookedseat);
+            return {
+                time:slot , 
+                availableseat , 
+                isavailable : availableseat>0 
+            }
+        })
+        res.json(availability)
+
     } catch (error : any) {
         console.log(error);
         res.status(400).json({message:error.message});
     }
-
 }
