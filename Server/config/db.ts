254102c917443e01043ca6c99c5343e17aa00c3d
Rishav250-cmd <rishavdev.05@gmail.com>
2026-08-30 05:04:40 +0530

    backend server completed

--- Server/config/db.ts
+++ Server/config/db.ts
@@ -4,5 +4,4 @@ const connectDB = async()=>{
     mongoose.connection.on("connected" , () => console.log("mongodb connected") );
     await mongoose.connect(process.env.MONGODB_URI!);
 }
-
 export default connectDB ; 
